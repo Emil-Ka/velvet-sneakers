db4c7d7319b9f4c46da6e395b4559046e91ba978
erkaraev <erkaraev@domclick.ru>
2023-04-17 08:02:36 +0300

    added ssr

--- src/app/providers/index.ts
+++
@@ -1,7 +0,0 @@
-import compose from 'compose-function';
-
-import { withStrictMode } from './with-strict-mode';
-import { withRouter } from './with-router';
-import { withTheme } from './with-theme';
-
-export const withProviders = compose(withStrictMode, withRouter, withTheme);
