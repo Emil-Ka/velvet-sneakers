eead5545ace6e9fda096245c96060f91f594e657
AndrewwKovv <kovshov.K@yandex.ru>
2023-04-18 21:09:22 +0300

    added footer and component text

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import cn from 'classnames';
 
-import { Container } from 'shared/components';
+import { Container, Text } from 'shared/components';
 
 import { HeaderProps } from './types';
 import styles from './header.module.scss';
@@ -9,7 +9,9 @@ import styles from './header.module.scss';
 export const Header: FC<HeaderProps> = ({ className, ...props }) => {
   return (
     <header className={cn(styles.header, className)} {...props}>
-      <Container>Header</Container>
+      <Container>
+        <Text>Header</Text>
+      </Container>
     </header>
   );
 };
