d9d4dc1325ff81973ba768e7c70cfc33fcee719e
Mikhail Sergeev <101133918+plamyaa@users.noreply.github.com>
2023-04-19 18:40:08 +0300

    Fix/carousel style (#6)
    fixed carousel styles

--- src/widgets/carousel/carousel.tsx
+++ src/widgets/carousel/carousel.tsx
@@ -3,36 +3,68 @@ import cn from 'classnames';
 
 import { CarouselProps } from './types';
 import styles from './carousel.module.scss';
+import useWindowDimensions from './helper';
 
 export const Carousel: FC<CarouselProps> = ({ className, perPage = 3, blocks }) => {
-  const [activeIndex, setActiveIndex] = useState(-1);
-  const [blockLength, setBlockLength] = useState(26);
+  const { width } = useWindowDimensions();
+  const [itemsPerPage, setItemsPerPage] = useState(perPage);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [blockLength, setBlockLength] = useState(32);
+  const [transformLenght, setTransformLength] = useState(74.2);
+
   useEffect(() => {
-    switch (perPage) {
+    if (width > 900) {
+      setItemsPerPage(perPage);
+    }
+    if (width > 670 && width < 900) {
+      if (itemsPerPage >= 2) {
+        setItemsPerPage(2);
+      } else {
+        setItemsPerPage(perPage);
+      }
+    }
+    if (width < 670) {
+      if (itemsPerPage >= 1) {
+        setItemsPerPage(1);
+      } else {
+        setItemsPerPage(perPage);
+      }
+    }
+
+    switch (itemsPerPage) {
       case 1:
-        setBlockLength(77);
+        setBlockLength(100);
+        setTransformLength(2);
         break;
       case 2:
-        setBlockLength(38);
+        setBlockLength(49);
+        setTransformLength(55.1);
         break;
       case 4:
-        setBlockLength(18.5);
+        setBlockLength(23.5);
+        setTransformLength(85);
         break;
       default:
-        setBlockLength(25);
+        setBlockLength(32);
+        setTransformLength(74.2);
         break;
     }
-  }, [perPage]);
+  }, [itemsPerPage, perPage, width]);
 
   const moveBack = () => {
-    if (activeIndex === 0) {
-      setActiveIndex(-blocks.length + 3);
+    const moveLenght = -(blocks.length - perPage);
+    const isStart = activeIndex === 0;
+    if (isStart) {
+      setActiveIndex(moveLenght);
     } else {
       setActiveIndex(activeIndex + 1);
     }
   };
+
   const moveNext = () => {
-    if (activeIndex === -blocks.length + 3) {
+    const moveLength = -(blocks.length - perPage);
+    const isEnd = activeIndex === moveLength;
+    if (isEnd) {
       setActiveIndex(0);
     } else {
       setActiveIndex(activeIndex - 1);
@@ -46,8 +78,8 @@ export const Carousel: FC<CarouselProps> = ({ className, perPage = 3, blocks }) 
           <div
             className={cn(styles.carouselItem)}
             style={{
-              transform: `translateX(${activeIndex * (blockLength + 1)}rem)`,
-              minWidth: `${blockLength}rem`,
+              transform: `translateX(${activeIndex * (blockLength + transformLenght)}%)`,
+              minWidth: `${blockLength}%`,
             }}
             key={block.id}
           >
@@ -64,36 +96,3 @@ export const Carousel: FC<CarouselProps> = ({ className, perPage = 3, blocks }) 
     </div>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
