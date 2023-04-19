d0df1a07429bb54660705da3d0d2acf053f8f2ab
Emil-Ka <emilkaraev55@gmail.com>
2023-04-19 09:33:33 +0300

    fixed carousel styles

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import { Carousel } from 'widgets/carousel';
+import { Container } from 'shared/components';
 
 import { Page } from '../page';
 import styles from './home.module.scss';
 
 export const Home = () => {
   return (
-    <Page className={styles.kefteme}>
-      Что за тяги такие бархатные? Уффф кефтемеее...
-      <Carousel blocks={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }]} />
+    <Page className={styles.home}>
+      <Container>
+        <Carousel blocks={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }]} />
+      </Container>
     </Page>
   );
 };
