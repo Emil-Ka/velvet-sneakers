0262b08a8ee2a9e07fa5252db3a13c53aaf92824
AndrewwKovv <kovshov.K@yandex.ru>
2023-04-19 16:06:09 +0300

    fix conf

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,2 +1,1 @@
 export { Container } from './container';
-export { Text } from './text';
--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import cn from 'classnames';
 
-import { Container, Text } from 'shared/components';
+import { Container } from 'shared/components';
 
 import { HeaderProps } from './types';
 import styles from './header.module.scss';
@@ -9,9 +9,7 @@ import styles from './header.module.scss';
 export const Header: FC<HeaderProps> = ({ className, ...props }) => {
   return (
     <header className={cn(styles.header, className)} {...props}>
-      <Container>
-        <Text>Header</Text>
-      </Container>
+      <Container>Header</Container>
     </header>
   );
 };
