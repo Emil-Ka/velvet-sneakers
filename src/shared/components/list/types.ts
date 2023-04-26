748c9c73f93826cb432ca018ed059da584a8d01e
AndrewwKovv <115148238+AndrewwKovv@users.noreply.github.com>
2023-04-26 18:57:00 +0300

    added pages about and contact (#10)
    added pages about and contact

---
+++ src/shared/components/list/types.ts
@@ -0,0 +1,5 @@
+import { ComponentProps, PropsWithChildren } from 'react';
+
+export type ListProps = PropsWithChildren<ComponentProps<'ul'>>;
+
+export type ListItemProps = PropsWithChildren<ComponentProps<'li'>>;
