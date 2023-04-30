52535719bc0a2cdd54088c3c2ec3b417693369c3
AndrewwKovv <115148238+AndrewwKovv@users.noreply.github.com>
2023-04-30 11:16:43 +0300

    Offer page (#14)
    added personal-page

--- src/shared/components/list/types.ts
+++ src/shared/components/list/types.ts
@@ -3,3 +3,7 @@ import { ComponentProps, PropsWithChildren } from 'react';
 export type ListProps = PropsWithChildren<ComponentProps<'ul'>>;
 
 export type ListItemProps = PropsWithChildren<ComponentProps<'li'>>;
+
+export interface ListWithIconProps extends ListItemProps {
+  type?: 'circle' | 'star';
+}
