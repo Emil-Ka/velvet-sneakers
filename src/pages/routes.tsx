f9c4e1908179b9963c143be8938d9b85cdea675c
Mikhail Sergeev <sergeevm100@gmail.com>
2023-05-01 19:19:39 +0300

    created page

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -7,12 +7,17 @@ import { Home } from './home';
 import { Contact } from './contact';
 import { About } from './about';
 import { BeforeAfter } from './before-after';
-import {Personal} from "./personal";
+import { Personal } from './personal';
+import { Shop } from './shop';
 
 const routes: RouteObject[] = [
   {
     path: PATHS.HOME,
     element: <Home />,
+  },
+  {
+    path: PATHS.SHOP,
+    element: <Shop />,
   },
   {
     path: PATHS.ABOUT_US,
