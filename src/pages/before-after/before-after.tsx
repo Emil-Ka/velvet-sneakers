023f2c319ce4e7de488dead2bc49bd55b81971d1
zavarts <zavoiskih.artyom@yandex.ru>
2023-05-03 23:06:36 +0300

    Integrated before-after page backend

--- src/pages/before-after/before-after.tsx
+++ src/pages/before-after/before-after.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'shared/components';
 import { Page } from 'pages/page';
-import { PRODUCTS } from './constants';
+import { IProduct } from 'shared/types/product';
 import styles from './before-after.module.scss';
+import { fetchProducts } from './api';
 
 // TODO добавить страницу в Header
 export const BeforeAfter = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  useEffect(() => {
+    let ignore = false;
+    setProducts([]);
+    fetchProducts().then((result) => {
+      if (!ignore) {
+        setProducts(result);
+      }
+    });
+    return () => {
+      ignore = true;
+    };
+  }, []);
   return (
     <Page>
       <Container className={styles.container}>
         <ul className={styles.products}>
-          {PRODUCTS.map(({ image }, index) => (
+          {products.map(({ image, title }, index) => (
             <li key={index}>
-              <img src={`assets/product/${image}`} alt={`Продукт${index}`} />
+              <img src={image} alt={title} />
             </li>
           ))}
         </ul>
--- src/pages/before-after/constants.ts
+++ src/pages/before-after/constants.ts
@@ -1,29 +1,40 @@
-export const PRODUCTS = [
+import { IProduct } from 'shared/types/product';
+
+export const PRODUCTS: IProduct[] = [
   {
-    image: 'Химчистка_кроссовок_4.png',
+    title: 'Химчистка_кроссовок_4.png',
+    image: 'assets/product/Химчистка_кроссовок_4.png',
   },
   {
-    image: 'Химчистка_кроссовок_10.png',
+    title: 'Химчистка_кроссовок_10.png',
+    image: 'assets/product/Химчистка_кроссовок_10.png',
   },
   {
-    image: 'Химчистка_кроссовок_11 1.png',
+    title: 'Химчистка_кроссовок_11 1.png',
+    image: 'assets/product/Химчистка_кроссовок_11 1.png',
   },
   {
-    image: 'Химчистка_кроссовок_15.png',
+    title: 'Химчистка_кроссовок_15.png',
+    image: 'assets/product/Химчистка_кроссовок_15.png',
   },
   {
-    image: 'Химчистка_кроссовок_25 1.png',
+    title: 'Химчистка_кроссовок_25 1.png',
+    image: 'assets/product/Химчистка_кроссовок_25 1.png',
   },
   {
-    image: 'Химчистка_кроссовок_26 1.png',
+    title: 'Химчистка_кроссовок_26 1.png',
+    image: 'assets/product/Химчистка_кроссовок_26 1.png',
   },
   {
-    image: 'Химчистка_кроссовок_29 1.png',
+    title: 'Химчистка_кроссовок_29 1.png',
+    image: 'assets/product/Химчистка_кроссовок_29 1.png',
   },
   {
-    image: 'Химчистка_кроссовок_30.png',
+    title: 'Химчистка_кроссовок_30.png',
+    image: 'assets/product/Химчистка_кроссовок_30.png',
   },
   {
-    image: 'Химчистка_кроссовок_33.png',
+    title: 'Химчистка_кроссовок_33.png',
+    image: 'assets/product/Химчистка_кроссовок_33.png',
   },
 ];
